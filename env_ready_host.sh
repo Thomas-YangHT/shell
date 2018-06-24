9e7f54627058cafc5c63dc728d4c60953f6f6ec2
Thomas yang <168447636@qq.com>
2018-06-25 00:56:57 +0800

    1
    1

--- env_ready_host.sh
+++ env_ready_host.sh
@@ -1,32 +1,40 @@
 #物理机安装：CentOS-7-x86_64-Minimal-1511.iso
 #注意启动修改initrd硬盘位置
+#此脚本安装 KVM虚拟化环境，并做基本系统设置
+#
 yum install -y epel-release wget
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 yum update
 yum install -y net-tools sysstat vim curl git chrony
-yum install -y docker docker-compose
 yum install -y qemu-kvm libvirt virt-install bridge-utils 
+yum install -y fabric ansible 
+#yum install -y docker docker-compose
 #yum install -y php php-mysql python-flask uwsgi uwsgi-plugin-python uwsgi-plugin-php MySQL-python
 #yum install -y awstats smokeping perl-CGI     
-yum install -y fabric ansible 
 #....  
 yum update
    
 #配置：
 systemctl stop firewalld
-systemctl stop NetworkManager
+#systemctl stop NetworkManager
 systemctl disable firewalld
-systemctl disable NetworkManager
-sed  -i  '/UseDNS/i UseeDNS=no' /etc/ssh/sshd_config
-sed -i.ori 's/rhgb/net.ifnames=0 biosdevnam=0 rhgb/g' /boot/grup2/grub.conf
+#systemctl disable NetworkManager
+#设置语言、字符集、时区、SSHD禁掉DNS解析、网卡的名字改回ethX;
+sed  -i  '/UseDNS/i UseDNS=no' /etc/ssh/sshd_config
+#sed -i.ori 's/rhgb/net.ifnames=0 biosdevnam=0 rhgb/g' /boot/grup2/grub.conf
+#单挂载/boot/efi的情况，grub.cfg位置变为：/boot/efi/EFI/centos/grub.cfg
+sed -i.ori 's/rhgb/net.ifnames=0 biosdevnam=0 rhgb/g' /boot/efi/EFI/centos/grub.cfg
 echo 'export PS1="\[\e[32;40m\]\u@\h\[\e[35;40m\]\t\[\e[0m\]\w#"' >>/root/.bashrc
 echo 'LANG="zh_CN.UTF-8"' >/etc/locale.conf
-echo 'LC_ALL="en_US.UTF-8"' >>/etc/locale.conf
+echo 'LC_ALL="zh_CN.UTF-8"' >>/etc/locale.conf
+#echo 'LC_ALL="en_US.UTF-8"' >>/etc/locale.conf
 ln -s  /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 #ntpdate xxx.xxx.xxx; clock -w
+#关闭selinux、打开IPV4转发、NTP服务
 sed -i.ori 's/enforcing/disabled/' /etc/selinux/config
 sed -i.ori -e '$a\bindcmdaddress 127.0.0.1\nbindcmdaddress ::1\nlocal stratum 10\n' /etc/chrony.conf
 sed -i.ori '$a\net.ipv4.ip_forward=1' /etc/sysctl.conf
+#修改IP为你的登陆主机，没有则忽略
 mkdir ~/.ssh;cd ~/.ssh;scp 192.168.31.140:/root/.ssh/id_rsa* .
 cat id_rsa.pub >>authorized_keys
 
