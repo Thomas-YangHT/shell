dac19cd5dfc23db8575734c30242c708b36f6339
Thomas yang <168447636@qq.com>
2018-07-05 15:03:36 +0800

    1

--- parted_yunwei.sh
+++ parted_yunwei.sh
@@ -8,5 +8,5 @@ parted /dev/sdb mkpart xfs 0G 3000G
 parted /dev/sdb mkpart xfs 3000G 3489G
 mkfs.xfs /dev/sdb1
 mkfs.xfs /dev/sdb2
-echo "/dev/sdb1    /mnt   xfs   default  0 0" >>/etc/fstab
-echo "/dev/sdb2    /opt   xfs   default  0 0" >>/etc/fstab
+echo "/dev/sdb1    /mnt   xfs   defaults  0 0" >>/etc/fstab
+echo "/dev/sdb2    /opt   xfs   defaults  0 0" >>/etc/fstab
