2fcc2e7b760de39f3a16e0094a3c8212c0ab1785
root <168447636@qq.com>
2018-07-13 23:45:58 +0800

    2

--- 6-local-repo.sh
+++ 6-local-repo.sh
@@ -11,9 +11,11 @@ mkdir -p $DIR/7/
 #createrepo -pdo /usr/local/apache/htdocs/x86_64 $DIR/os
 #createrepo -pdo /usr/local/apache/htdocs/extra  $DIR/extra
 #createrepo -pdo /usr/local/apache/htdocs/update $DIR/updates
+killall rsync
 rsync -avrt --delete $SOURCE/7/extras  $DIR/7
 rsync -avrt --delete $SOURCE/7/os      $DIR/7
 rsync -avrt --delete $SOURCE/7/updates $DIR/7
 rsync -avrt --delete $SOURCE/7/centosplus $DIR/7
 rsync -avrt --delete $SOURCE/RPM-GPG-KEY-CentOS-7 $DIR
 #createrepo --update /usr/local/apache/htdocs/centos/extra
+reposync --repoid=openstack-ocata
