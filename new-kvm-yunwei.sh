0d9e7be2eae0f23a5072724befe9c9f306eb062a
root <168447636@qq.com>
2018-07-15 13:55:41 +0800

    2

---
+++ new-kvm-yunwei.sh
@@ -0,0 +1,16 @@
+#new kvm
+if [ "$1" ];then
+  kvmname=$1
+  kvmbase="192.168.254.254"
+  touch ~/.ssh/config && echo -e "StrictHostKeyChecking=no\nUserKnownHostsFile=/dev/null" >> ~/.ssh/config
+  ssh $kvmbase hostnamectl set-hostname $kvmname
+  ssh $kvmbase ifconfig eth0 172.16.253.$(($RANDOM+1/254))
+  ssh $kvmbase ifconfig eth2
+  ssh $kvmbase "curl 192.168.254.5/shell/docker-registry-yunwei.sh|bash"
+else
+  echo "Usage: sh new-kvm-yunwei.sh KVMXXX"
+fi
+
+
+
+
