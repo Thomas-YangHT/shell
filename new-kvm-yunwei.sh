ccdcb16046b99ca3b11bae9d09f06bb9c1d99878
root <168447636@qq.com>
2018-07-15 15:10:14 +0800

    2

--- new-kvm-yunwei.sh
+++ new-kvm-yunwei.sh
@@ -1,11 +1,17 @@
 #new kvm
+#set hostname & ip/dns/gw
+#
 if [ "$1" ];then
   kvmname=$1
-  kvmbase="192.168.254.254"
+  kvmbase="192.168.253.254"
+  SUFFIX="$((($RANDOM+1)/254))"
   touch ~/.ssh/config && echo -e "StrictHostKeyChecking=no\nUserKnownHostsFile=/dev/null" >> ~/.ssh/config
   ssh $kvmbase hostnamectl set-hostname $kvmname
-  ssh $kvmbase ifconfig eth0 172.16.253.$(($RANDOM+1/254))
+  ssh $kvmbase ifconfig eth0 172.16.253.$SUFFIX
+  ssh $kvmbase ifconfig eth1 192.168.250.$SUFFIX
   ssh $kvmbase ifconfig eth0
+  ssh $kvmbase hostname
+  ssh $kvmbase "curl 192.168.254.5/shell/sys_baseinfo-yunwei.sh|bash"
   #ssh $kvmbase "curl 192.168.254.5/shell/docker-registry-yunwei.sh|bash"
 else
   echo "Usage: sh new-kvm-yunwei.sh KVMXXX"
--- sys_baseinfo-yunwei.sh
+++ sys_baseinfo-yunwei.sh
@@ -2,6 +2,7 @@
 #
 #sys_baseinfo.sh:   Get sys_base_info and report to CMDB
 #
+MYHOST="192.168.250.211"
 HOSTNAME=`hostname`
 TZ=`tail -n 1 /etc/localtime`
 KERNEL=`uname -ro`
@@ -16,4 +17,4 @@ echo $HOSTNAME, $KERNEL, $TZ, $MAC, $IP, $CPU, $MEMORY, $DISK, $SERIESNO
 
 sql="insert into baseinfo(hostname, kernel, tz, mac, ip, cpu, memory, disk, seriesno) \
      select \"$HOSTNAME\",\"$KERNEL\",\"$TZ\", \"$MAC\", \"$IP\", \"$CPU\",  \"$MEMORY\", \"$DISK\", \"$SERIESNO\" "
-echo $sql|mysql -uyanght -D monitor -pyanght -h 192.168.254.211
+echo $sql|mysql -uyanght -D monitor -pyanght -h $MYHOST
