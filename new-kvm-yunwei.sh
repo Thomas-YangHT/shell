19bbd3ddba3fbd2f6605bcbc09bd717ab70ac2bd
root <168447636@qq.com>
2018-07-17 21:51:21 +0800

    2

--- new-kvm-yunwei.sh
+++ new-kvm-yunwei.sh
@@ -9,6 +9,7 @@ if [ "$1" ];then
   ssh $kvmbase hostnamectl set-hostname $kvmname
   ssh $kvmbase ifconfig eth0 172.16.253.$SUFFIX
   ssh $kvmbase ifconfig eth1 192.168.251.$SUFFIX
+  ssh $kvmbase ifconfig route add -net 192.168.251.0/24 dev eth1
   ssh $kvmbase ifconfig eth0
   ssh $kvmbase hostname
   ssh $kvmbase "curl 192.168.251.5/shell/sys_baseinfo-yunwei.sh|bash"
