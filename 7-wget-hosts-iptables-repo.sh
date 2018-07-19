eb17eb5a089df8189ecdf5ea7ca48f4f640ac47f
root <168447636@qq.com>
2018-07-19 10:35:28 +0800

    2

--- 7-wget-hosts-iptables-repo.sh
+++ 7-wget-hosts-iptables-repo.sh
@@ -8,9 +8,9 @@ wget -O /usr/libexec/iptables/iptables.init  download.yunwei.edu/shell/iptables.
 wget -O /usr/lib/systemd/system/iptables.service  download.yunwei.edu/shell/iptables.service
 systemctl daemon-reload
 systemctl restart iptables
-#wget -O /etc/yum.repos.d/Centos7-Base-yunwei.repo    download.yunwei.edu/shell/Centos7-Base-yunwei.repo  
-#wget -O /etc/yum.repos.d/epel-yunwei.repo   download.yunwei.edu/shell/epel-yunwei.repo
-#wget -O /etc/yum.repos.d/rdo-release-yunwei.repo  download.yunwei.edu/shell/rdo-release-yunwei.repo
-#wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Cloud  download.yunwei.edu/shell/RPM-GPG-KEY-CentOS-SIG-Cloud
-#cd /etc/yum.repos.d/;mv epel.repo ..;mv CentOS-Base.repo ..;mv rdo-release.repo ..;
-#yum repolist
+wget -O /etc/yum.repos.d/Centos7-Base-yunwei.repo    download.yunwei.edu/shell/Centos7-Base-yunwei.repo  
+wget -O /etc/yum.repos.d/epel-yunwei.repo   download.yunwei.edu/shell/epel-yunwei.repo
+wget -O /etc/yum.repos.d/rdo-release-yunwei.repo  download.yunwei.edu/shell/rdo-release-yunwei.repo
+wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Cloud  download.yunwei.edu/shell/RPM-GPG-KEY-CentOS-SIG-Cloud
+cd /etc/yum.repos.d/;mv epel.repo ..;mv CentOS-Base.repo ..;mv rdo-release.repo ..;
+yum repolist
---
+++ sys_baseinfo-yunwei-em1.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+#
+#sys_baseinfo.sh:   Get sys_base_info and report to CMDB
+#
+MYHOST="192.168.251.211"
+ping 192.168.251.211 -c 1 && MYHOST=192.168.251.211 ||  MYHOST=192.168.254.211;echo $MYHOST
+
+export LC_ALL=en_US.UTF-8
+HOSTNAME=`hostname`
+TZ=`tail -n 1 /etc/localtime`
+KERNEL=`uname -ro`
+MAC=`ifconfig em1|grep ether|awk '{print $2}'`
+IP=`ifconfig em1| sed -n 's/inet\(.*\)net\(.*\)/\1/p'`
+CPU=`cat /proc/cpuinfo |grep 'model name'|awk -F':' '{print $2}'|uniq -c`
+MEMORY=`cat /proc/meminfo |grep MemTotal`
+DISK=`fdisk -l |grep -v mapper|grep -Po 'Disk /dev/\K.*GB'|xargs`
+SERIESNO=`dmidecode -s system-serial-number`
+
+echo $HOSTNAME, $KERNEL, $TZ, $MAC, $IP, $CPU, $MEMORY, $DISK, $SERIESNO
+
+sql="insert into baseinfo(hostname, kernel, tz, mac, ip, cpu, memory, disk, seriesno) \
+     select \"$HOSTNAME\",\"$KERNEL\",\"$TZ\", \"$MAC\", \"$IP\", \"$CPU\",  \"$MEMORY\", \"$DISK\", \"$SERIESNO\" "
+echo $sql|mysql -uyanght -D monitor -pyanght -h $MYHOST
