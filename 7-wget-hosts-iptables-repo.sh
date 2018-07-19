603e23b537de47bb682a689e6f09c378d47bfb5e
root <168447636@qq.com>
2018-07-19 11:25:34 +0800

    2

--- 7-wget-hosts-iptables-repo.sh
+++ 7-wget-hosts-iptables-repo.sh
@@ -2,10 +2,12 @@ cat <<EOF>/etc/resolv.conf
 nameserver  192.168.254.251
 nameserver  223.5.5.5
 EOF
+mkdir /usr/libexec/iptables
 wget -O /etc/hosts download.yunwei.edu/shell/hosts
 wget -O /etc/sysconfig/iptables  download.yunwei.edu/shell/iptables
 wget -O /usr/libexec/iptables/iptables.init  download.yunwei.edu/shell/iptables.init 
 wget -O /usr/lib/systemd/system/iptables.service  download.yunwei.edu/shell/iptables.service
+chmod +x /usr/libexec/iptables/iptables.init
 systemctl daemon-reload
 systemctl restart iptables
 wget -O /etc/yum.repos.d/Centos7-Base-yunwei.repo    download.yunwei.edu/shell/Centos7-Base-yunwei.repo  
