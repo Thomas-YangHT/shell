fea040b5f8d28d7b606ddaa2246e2bc4c9110ba1
root <168447636@qq.com>
2018-08-05 22:22:40 +0800

    2

--- lvs_dr_rs.sh
+++ lvs_dr_rs.sh
@@ -4,8 +4,8 @@ vip=172.16.254.246
 vip2=172.16.254.247
 ifconfig lo:0 $vip broadcast $vip netmask 255.255.255.255 up
 route add -host $vip lo:0
-ifconfig lo:0 $vip2 broadcast $vip netmask 255.255.255.255 up
-route add -host $vip2 lo:0
+ifconfig lo:1 $vip2 broadcast $vip2 netmask 255.255.255.255 up
+route add -host $vip2 lo:1
 echo "1" >/proc/sys/net/ipv4/conf/lo/arp_ignore
 echo "2" >/proc/sys/net/ipv4/conf/lo/arp_announce
 echo "1" >/proc/sys/net/ipv4/conf/all/arp_ignore
