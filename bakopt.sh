e2f746e7c5f6818fc7e49123c058ddb7ceadceaf
Thomas yang <168447636@qq.com>
2018-09-09 20:13:15 +0800

    1

--- bakopt.sh
+++ bakopt.sh
@@ -1,3 +1,4 @@
+
 DDATE=`date +%Y%m%d-%H%M%S`
 KEEPDAY=14
 BAKDIR='/export/backup'
@@ -6,10 +7,12 @@ BAKDIR='/export/backup'
 #backup yunwei opt110
 mount -t nfs4 192.168.254.251:/ /export/backup/mnt
 sleep 3
-tar zcvf /export/backup/opt110_$DDATE.tgz --exclude=*/nginxlog/* --exclude=*/download/* --exclude=*/mysql/* --exclude=*/mysql_binlog/*  /export/backup/mnt/*
+tar zcvf /export/backup/opt110_$DDATE.tgz --exclude=*/nginxlog/* --exclude=*/download/* --exclude=*/mysql/* --exclude=*/mysql_binlog/*
+  /export/backup/mnt/*
 umount /export/backup/mnt
 #backup yunwei opt 5
-tar zcvf /export/backup/opt5_$DDATE.tgz.tgz --exclude=*/nginxlog/* --exclude=*/download/* --exclude=*/mysql/* --exclude=*/mysql_binlog/*  /opt/*
+tar zcvf /export/backup/opt5_$DDATE.tgz.tgz --exclude=*/nginxlog/* --exclude=*/download/* --exclude=*/mysql/* --exclude=*/mysql_binlog
+/*  /opt/*
 
 #backup mysql
 /usr/bin/mysqldump -uyanght -pyanght -h 192.168.254.251 --databases monitor moninfo docker cmdb zabbix >./mysql_$DDATE.sql
