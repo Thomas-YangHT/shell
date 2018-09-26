632633510aa2d2f846ae83e7467462bbe0112bf1
Thomas yang <168447636@qq.com>
2018-09-26 20:40:19 +0800

    1

--- ngx_download.sh
+++ ngx_download.sh
@@ -4,6 +4,7 @@ DIR=" /opt/cmp_nginx_download";   mkdir  $DIR &&  cd $DIR
 wget https://raw.githubusercontent.com/Thomas-YangHT/docker-compose/master/ngx_download/docker-compose.yml
 #注意compose文件做相应修改
 
+wget  https://raw.githubusercontent.com/Thomas-YangHT/docker-compose/master/ngx_download/nginx.conf
 wget  https://raw.githubusercontent.com/Thomas-YangHT/docker-compose/master/ngx_download/download.yunwei.edu.conf
 #注意修改conf文件中是域名以适应你的情况
 
