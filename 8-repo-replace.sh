b71e12b19f2161f6fa14335dcded146edf6d1640
Thomas yang <168447636@qq.com>
2018-09-26 16:55:46 +0800

    1

--- 8-repo-replace.sh
+++ 8-repo-replace.sh
@@ -1,6 +1,6 @@
+cd /etc/yum.repos.d/;mv *repo ..;
 wget -O /etc/yum.repos.d/Centos7-Base-yunwei.repo    download.yunwei.edu/shell/Centos7-Base-yunwei.repo  
 wget -O /etc/yum.repos.d/epel-yunwei.repo   download.yunwei.edu/shell/epel-yunwei.repo
 wget -O /etc/yum.repos.d/rdo-release-yunwei.repo  download.yunwei.edu/shell/rdo-release-yunwei.repo
 wget -O /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Cloud  download.yunwei.edu/shell/RPM-GPG-KEY-CentOS-SIG-Cloud
-cd /etc/yum.repos.d/;mv *repo ..;
 yum repolist
