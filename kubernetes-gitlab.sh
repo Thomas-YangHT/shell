885fb43b79261d5e65532d09bc0a98be61f58a96
Thomas yang <168447636@qq.com>
2018-10-15 23:49:54 +0800

    1

--- kubernetes-gitlab.sh
+++ kubernetes-gitlab.sh
@@ -4,11 +4,15 @@ http://gitlab.yunwei.edu/help/user/project/clusters/index.md#installing-applicat
 
 
 #https://blog.csdn.net/weiguang1017/article/details/78045013
+wget https://storage.googleapis.com/kubernetes-helm/helm-v2.6.1-linux-amd64.tar.gz
+#解包并将二进制文件helm拷贝到/usr/local/bin目录下
+tar -zxvf helm-v2.6.1-linux-amd64.tgz && mv linux-amd64/helm /usr/local/bin/helm
 helm init --upgrade -i registry.cn-hangzhou.aliyuncs.com/google_containers/tiller:v2.11.0 --stable-repo-url https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
 
 helm install --name my-mariadb --set Persistence.StorageClass=slow stable/mariadb 
-（不加--set参数会出现SchedulerPredicates failed due to PersistentVolumeClaim is not bound: "my-release-jenkins", which is unexpected问题）
+（不加--set参数会出现SchedulerPredicates failed due to PersistentVolumeClaim is not bound: "", which is unexpected问题）
 
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/nginx-ingress-controller:0.20.0
