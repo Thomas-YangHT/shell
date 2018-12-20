627fc079600855ec7ddc1c3584695b03c59c8ca0
root <168447636@qq.com>
2018-12-20 11:43:24 -0500

    2

---
+++ set_local_repo.sh
@@ -0,0 +1,7 @@
+cd /etc/yum.repos.d/;mv * ..
+curl -o /etc/yum.repos.d/Centos7-Base-yunwei.repo    download.yunwei.edu/config/CentOS-Base-yunwei.repo  
+curl -o /etc/yum.repos.d/epel-yunwei.repo   download.yunwei.edu/config/epel-yunwei.repo
+#curl -o /etc/yum.repos.d/rdo-release-yunwei.repo  download.yunwei.edu/config/rdo-release-yunwei.repo
+#curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-SIG-Cloud  download.yunwei.edu/config/RPM-GPG-KEY-CentOS-SIG-Cloud
+curl -o /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7  download.yunwei.edu/config/RPM-GPG-KEY-EPEL-7
+yum repolist
